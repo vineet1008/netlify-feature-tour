b1694fbe29e42ba464c0dbbb59b2f1df5ee83e16
Vineet <guptavineet287@gmail.com>
2024-08-04 15:33:09 +0530

    -Updated on-04-08-2024

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
